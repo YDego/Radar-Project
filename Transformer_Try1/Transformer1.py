2c6a818c6c00227f86a8ad04e5c7947d6a09de6f
YDego <yehonatand123@gmail.com>
2023-01-05 13:37:45 +0200

    trasnsformer model ready to run output to midi available

--- Transformer_Try1/Transformer1.py
+++
@@ -1,8 +0,0 @@
-from mido import MidiFile
-
-mid = MidiFile('MIDI Samples\Mullen - Solace (Midi Kit)\M. Almost there - 140 BPM.mid', clip=True)
-print(mid)
-for track in mid.tracks:
-    print(track)
-for msg in mid.tracks[0]:
-    print(msg)
