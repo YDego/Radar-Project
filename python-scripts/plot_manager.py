729fd9c5d812d7c72f1157c7c45872aa70e4d5d8
YDego <yehonatand123@gmail.com>
2024-01-23 12:05:27 +0200

    fix

--- python-scripts/plot_manager.py
+++ python-scripts/plot_manager.py
@@ -26,16 +26,13 @@ def plot_amp_n_phase(w, f, amp, phase, f_start, f_end, fs, z_dist):
     fig, axs = plt.subplots(2)
     fig.suptitle('Amplitude and Phase Graphs (z={}[m])'.format(z_dist))
 
-    w_start_idx = int(f_start * 2 * math.pi / fs)
-    w_end_idx = int(f_end * 2 * math.pi / fs)
-
     f_start_idx = int(f_start / fs)
     f_end_idx = int(f_end / fs)
 
-    axs[0].plot(f[f_start_idx:f_end_idx], 20*np.log(amp)[w_start_idx:w_end_idx], 'tab:blue')
+    axs[0].plot(f[f_start_idx:f_end_idx], 20*np.log(amp)[f_start_idx:f_end_idx], 'tab:blue')
     axs[0].set(xlabel='frequency [MHz]', ylabel='Amplitude [dB]')
 
-    axs[1].plot(f[f_start_idx:f_end_idx], phase[w_start_idx:w_end_idx], 'tab:red')
+    axs[1].plot(f[f_start_idx:f_end_idx], phase[f_start_idx:f_end_idx], 'tab:red')
     axs[1].set(xlabel='frequency [MHz]', ylabel='Phase [rad]')
 
     plt.show()
