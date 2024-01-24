a728065a4ff2ec80bae8ff8e8572b1c5f4546f02
YDego <yehonatand123@gmail.com>
2024-01-24 14:05:32 +0200

    in progress

--- python-scripts/plot_manager.py
+++ python-scripts/plot_manager.py
@@ -25,9 +25,13 @@ def plot_amp_n_phase(f, amp, phase):
     fig, axs = plt.subplots(2)
     fig.suptitle('Amplitude and Phase Graphs (z={}[m])'.format(r))
 
-    half_n = int(N/2)
-    start_idx = int()
-    end_idx = int(N)
+    half_n = int(N/2) + 1
+    f = f[half_n:]
+    amp = amp[half_n:]
+    phase = phase[half_n:]
+
+    start_idx = int(f_start * half_n / BANDWIDTH)
+    end_idx = int(f_end * half_n / BANDWIDTH)
 
     axs[0].plot(f[start_idx:end_idx], 20*np.log(amp)[start_idx:end_idx], 'tab:blue')
     axs[0].set(xlabel='frequency [MHz]', ylabel='Amplitude [dB]')
